71736363738c17950accb0dc90f35d01bc8cf684
Your Name <you@example.com>
2024-01-10 10:20:29 +0200

    paper

--- rps.py
+++ rps.py
@@ -6,9 +6,23 @@
 # 3. Atkarībā no ievadītajiem cipariem un datora, izdrukā paziņojumu par zaudējumu, uzvaru, vai neizšķirtu (izmanto if, else https://www.w3schools.com/python/python_conditions.asp)
 #
 
+import random
+
 ROCK = "1"
 SCISSOR = "2"
 PAPER = "3"
 
 print("Welcome to Rock, Paper, Scissors!")
-
+user_choice = input("Enter 1 for Rock, 2 for Scissor, or 3 for Paper: ")
+computer_choice = str(random.randint(1, 3))
+print("Computer chose:", computer_choice)
+if user_choice == computer_choice:
+    print("It's a tie!")
+elif (
+    (user_choice == ROCK and computer_choice == SCISSOR)
+    or (user_choice == SCISSOR and computer_choice == PAPER)
+    or (user_choice == PAPER and computer_choice == ROCK)
+):
+    print("You win!")
+else:
+    print("You lose!")
